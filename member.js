53003ab60aa73d8b9f1fc76625fc48b015c846a3
Pankaj Khandelwal <pankaj139@gmail.com>
2024-07-17 09:43:27 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillMembers() {
+    let members = [
+        {name: 'John', skill: 'JavaScript'},
+        {name: 'Jane', skill: 'JavaScript'},
+        {name: 'Jack', skill: 'TypeScript'},
+        {name: 'Jill', skill: 'Python'}
+    ];
+    return members.filter(member => member.skill === 'JavaScript');
+}
